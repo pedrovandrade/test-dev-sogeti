2b99c8f471cf38eb74a133f495798570ca74de3b
pedrovandrade <pedrovandrade@gmail.com>
2022-02-02 05:32:08 +0100

    User Story #4: Add a new TODO

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,37 @@ import TodoList from './TodoList';
 export default function App(props) {
   /** ******** REACT HOOKS ********* */
   const [listData, setListData] = useState(props.listData || []);
-  
+  const [newTodo, setNewTodo] = useState('');
+
+  function addTodo() {
+    let newUuid = crypto.randomUUID();
+    let alreadyExists = !!props.listData.find(item => item.uuid === newUuid);
+    while (alreadyExists) {
+      newUuid = crypto.randomUUID();
+      alreadyExists = !!props.listData.find(item => item.uuid === newUuid);
+    }
+
+    setListData(oldArray => [...oldArray, {
+      "title": newTodo,
+      "state": "todo",
+      "description": "",
+      "uuid": newUuid
+    }]);
+
+    setNewTodo('');
+  }
+
   return (
     <div className='list-container' >
       <h1 className='list-title' >My todo list</h1>
-
-      <input type='text' placeholder='What do you want to do?' />
-      <button id='add-button' onClick={() => console.log('Button clicked!')} >Add</button>
-
+      <
+        input
+        type='text'
+        placeholder='What do you want to do?'
+        value={newTodo}
+        onInput={(e) => setNewTodo(e.target.value)}
+      />
+      <button id='add-button' onClick={addTodo}>Add</button>
       <TodoList listData={listData} setListData={setListData} />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -19,5 +19,38 @@ axios.get('/public/data/todo-list.json', { responseType: 'json' }).then((respons
     document.getElementById('root'),
   );
 }).catch((err) => {
-  throw new Error(err);
+  // throw new Error(err);
+  const response = [
+    {
+      "title": "Do groceries",
+      "state": "todo",
+      "description": "Go to the supermarket and buy eggs, butter, sugar, fruits, soap and toillete paper",
+      "uuid": "8348db18-83ba-11ec-a8a3-0242ac120002"
+    },
+    {
+      "title": "Wash car",
+      "state": "todo",
+      "description": "Take the car to the gas station and leave it there for 30 minutes in the express washing",
+      "uuid": "9c377382-83ba-11ec-a8a3-0242ac120002"
+    },
+    {
+      "title": "Clean house",
+      "state": "todo",
+      "description": "Pass vaccum, clean the surfaces, clean the toillete and pass water on the floor",
+      "uuid": "a081b5ec-83ba-11ec-a8a3-0242ac120002"
+    }
+  ];
+
+  ReactDOM.render(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App listData={response.data} />} />
+        <Route path='/details/*' element={<Details listData={response.data} />} />
+        <Route path='*' element={
+          <main><h2>Error 404</h2><p>URL not found</p></main>
+        } />
+      </Routes>
+    </BrowserRouter>,
+    document.getElementById('root'),
+  );
 });
